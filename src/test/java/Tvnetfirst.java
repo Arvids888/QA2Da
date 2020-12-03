607ebea132138c2c6aed24843955fc7c648efeef
44757 <arvidsarbidans@inbox.lv>
2020-12-03 09:57:53 -0800

    Seventh

--- src/test/java/Tvnetfirst.java
+++ src/test/java/Tvnetfirst.java
@@ -14,22 +14,21 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.List;
 
 public class Tvnetfirst {
-    //private final By COOKIE = By.xpath(".//button[contains(@class, 'dvvOSu')]");
 
     private final By ARTICLE = By.tagName("article");
+    private final String ARTICLE_BY_TITLE = "Konflikts Etiopijā: kas un kāpēc par to ir jāzina?";
     private final By TITLE = By.xpath(".//span[contains(@class, 'article__headline')]");
     private final By COMMENTS_COUNT = By.xpath(".//span[contains(@class, 'article__comment')]");
 
     private final By ARTICLE_TITLES_AND_COMMENTS = By.tagName("article");
     private final By ARTICLE_PAGE_TITLE = By.xpath(".//h1[contains(@class, 'article-headline')]");
     private final By ARTICLE_COMMENTS_COUNT = By.xpath(".//span[(@class = 'article-share__item--count')]");
+    private final By ARTICLE_COMMENTS_BUTTON = By.xpath(".//a[contains(@class, 'article-share__item--comments')]");
 
-    //private final By COMMENT_PAGE = By.tagName("main");
     private final By COMMENT_PAGE_TITLE = By.xpath(".//h1[(@class = 'article-headline')]");
     private final By COMMENT_PAGE_COMMENTS_COUNT = By.xpath(".//span[contains(@class, 'article-comments-heading')]");
-
-    private final Logger logger = LogManager.getLogger(this.getClass());
-
+    private final By COMMENT_PAGE_USER_COMMENT_COUNT = By.xpath(".//li[contains(@id, 'comment-')]");
+    private final By COMMENT_PAGE_LOGO = By.xpath(".//a[contains(@class, 'menu-item section')]");
 
     private WebDriver driver;
 
@@ -40,15 +39,6 @@ public class Tvnetfirst {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://www.tvnet.lv/");
-
-        //wait for cookie
-        //WebDriverWait wait = new WebDriverWait(driver, 10);
-        //wait.until(ExpectedConditions.visibilityOfElementLocated(COOKIE));
-
-        //get cookie
-        //String cookieTab = driver.findElement(COOKIE).getText();
-        //driver.findElement(COOKIE).click();
-
     }
 
     @Test
@@ -58,7 +48,7 @@ public class Tvnetfirst {
         List<WebElement> articles = driver.findElements(ARTICLE);
 
         //get correct article
-        WebElement article = articles.get(5);
+        WebElement article = articles.get(3);
 
         //get article
         String homePageTitle = article.findElement(TITLE).getText();
@@ -72,10 +62,10 @@ public class Tvnetfirst {
         //click on it
         article.findElement(TITLE).click();
 
-
         //wait for title
         WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.visibilityOfElementLocated(ARTICLE_PAGE_TITLE));
+
 
         //get all articles
         List <WebElement> articleTitles = driver.findElements(ARTICLE_TITLES_AND_COMMENTS);
@@ -86,6 +76,9 @@ public class Tvnetfirst {
         //get article
         String articlePageTitle = articleTitle.findElement(ARTICLE_PAGE_TITLE).getText();
 
+        //press comments button
+        WebElement commentsButton = driver.findElement(ARTICLE_COMMENTS_BUTTON);
+
         //get all comments
         List <WebElement> articleComments = driver.findElements(ARTICLE_COMMENTS_COUNT);
 
@@ -93,21 +86,25 @@ public class Tvnetfirst {
         WebElement articleComment = articleComments.get(1);
 
         //find comment count
-        if (!articleComment.findElements(ARTICLE_COMMENTS_COUNT).isEmpty()) {
-            articleComment = (articleComment.findElement(ARTICLE_COMMENTS_COUNT));
-        }
 
         //acquiring comments and turning into Integer
-        int articleIntComments;
-        String articleRightComments = articleComment.getText();
-        articleIntComments = parseCommentCountFour(articleRightComments);
+        int articleIntComments = 0;
+        String rightComments = articleComment.getText();
+        articleIntComments = parseCommentCountFour(rightComments);
 
         //check it
         Assertions.assertTrue(homePageTitle.startsWith(articlePageTitle), "Titles are not equal!");
         Assertions.assertEquals(homePageCommentsCount, articleIntComments, "Wrong comment amount");
 
         //open comments page
-        articleComment.click();
+        if (!articleComment.findElements(ARTICLE_COMMENTS_COUNT).isEmpty()) {
+            articleComment = (articleComment.findElement(ARTICLE_COMMENTS_COUNT));
+            articleComment.click();
+        }
+        else {
+            commentsButton.click();
+        }
+
 
         //wait for title
         WebDriverWait wait2 = new WebDriverWait(driver, 10);
@@ -120,35 +117,45 @@ public class Tvnetfirst {
         //get comments
         WebElement commentPageComments = driver.findElement(COMMENT_PAGE_COMMENTS_COUNT);
 
+        if (commentPageComments.findElements(COMMENT_PAGE_COMMENTS_COUNT).isEmpty()) {
+            closeBrowser();
+        }
+
+
         //acquiring comments and turning into Integer
-        int CommentsPageIntComments;
-        String CommentsPageRightComments = commentPageComments.getText();
-        CommentsPageIntComments = parseCommentCountFour(articleRightComments);
+        int CommentsPageIntComments = 0;
+        CommentsPageIntComments = parseCommentCountFour(rightComments);
+
+        //List<WebElement> optionCount = driver.findElements(COMMENT_PAGE_USER_COMMENT_COUNT);
+
 
         //check it
         Assertions.assertTrue(homePageTitle.startsWith(commentPageTitle), "Titles are not equal!");
         Assertions.assertEquals(homePageCommentsCount, CommentsPageIntComments, "Wrong comment amount");
 
+        //back to main page
+        List <WebElement> logo = driver.findElements(COMMENT_PAGE_LOGO);
+        WebElement logoFirst = logo.get(0);
+        logoFirst.click();
+
+        boolean isFound = false;
+        for (WebElement textArticle : articles) {
+            if (textArticle.findElement(TITLE).getText().equals(ARTICLE_BY_TITLE)) {
+                textArticle.findElement(TITLE).click();
+                isFound = true;
+            }
+        }
+
 
     }
-    private int parseCommentCountOne(String textToParse) {
-        textToParse = textToParse.substring(11, textToParse.length() - 1);
-        return Integer.parseInt(textToParse);
-    }
-
     private int parseCommentCountTwo(String textToParse) {
         textToParse = textToParse.substring(1, textToParse.length() - 1);
         return Integer.parseInt(textToParse);
     }
-    private int parseCommentCountThree(String textToParse) {
-        textToParse = textToParse.substring(12, textToParse.length() - 1);
-        return Integer.parseInt(textToParse);
-    }
+
     private int parseCommentCountFour(String textToParse) {
         return Integer.parseInt(textToParse);
     }
-
-
 
     @AfterEach
     private void closeBrowser() {
